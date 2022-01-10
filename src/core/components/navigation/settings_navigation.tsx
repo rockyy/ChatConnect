be1c81b2abc3d115bace72a7b71c09a02e3145dd
Rakesh Kumar <Rakeshkumar.sgnr@gmail.com>
2022-01-10 10:37:10 +0100

    settings localization updates

--- src/core/components/navigation/settings_navigation.tsx
+++ src/core/components/navigation/settings_navigation.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {useTranslation} from 'react-i18next';
 import {SettingsScreen, Privacy, Language} from '@settings/screens';
 
 const SettingsStack = createStackNavigator();
 
 export const SettingsNavigator: React.FC = () => {
+  const {t} = useTranslation(['settings']);
   const {Navigator, Screen} = SettingsStack;
 
   return (
@@ -12,9 +14,9 @@ export const SettingsNavigator: React.FC = () => {
       screenOptions={{
         headerShown: true,
       }}>
-      <Screen name='Settings' component={SettingsScreen} options={{title: ''}} />
-      <Screen name='Privacy' component={Privacy} />
-      <Screen name='Language' component={Language} />
+      <Screen name='Settings' component={SettingsScreen} options={{headerTitle: ''}} />
+      <Screen name='Privacy' component={Privacy} options={{headerTitle: t('privacy policy')}} />
+      <Screen name='Language' component={Language} options={{headerTitle: t('language')}} />
     </Navigator>
   );
 };
